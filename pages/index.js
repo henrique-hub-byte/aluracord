5c6ad40d3f301f335a398c0cdeecffe957485670
luis henrique <luis75henrique75@gmail.com>
2022-01-25 22:05:02 -0300

    atualizando

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,55 @@
-
-import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
 
-
-
-function GlobalStyle(){
-    return (
-        <style global jsx>{`
-            * {
-                
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style:none
-            }     
-            
-            body {
-                font-family: 'Open sans';
-            }
-            /* App fit height */ 
-            html,body #_next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #_next {
-                flex 1;
-            }
-
-            #_next > * {
-                flex: 1;
-            }
-            /* ./App fit height */
-        `}</style>
-    );
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
+  );
 }
 
-function Title(props){
-    const Tag = props.tag;
-    return (
-        <>
-        <Tag> {props.children} </Tag>
-        <style jsx>{`
-                ${Tag}{
-                    color: ${appConfig.theme.colors.neutrals['900']};
-                    font-size: 24px;
-                    font-weight: 600;
-                }
+
+
+
+function Title(props) {
+  const Tag = props.tag || 'h1';
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+            ${Tag} {
+                color: ${appConfig.theme.colors.neutrals['000']};
+                font-size: 24px;
+                font-weight: 600;
+            }
             `}</style>
-        </>
-    );
+    </>
+  );
 }
+
+
 
 //componente react
 /* function HomePage() {
